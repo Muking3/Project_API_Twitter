2a61bdb048652bacf3bc5126c65d27eda3ed9163
Muking3 <marcmusasa04@gmail.com>
2024-02-26 17:36:30 +0200

    delete comment

--- src/Middlewares/AccountValidation.js
+++ src/Middlewares/AccountValidation.js
@@ -4,13 +4,10 @@ const schema = Joi.object({
     name: Joi.string().min(3).max(15),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(30).required(),
-    // pseudo: Joi.string().max(8)
 });
 
 export const AccountVerify = (req, res, next) => {
-    // console.log(req);
     const { error } = schema.validate(req.body, { abortEarly: false });
-    // uploadProfil()
     if (error) {
         return res.status(400).send(error.details);
     }
